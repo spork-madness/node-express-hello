d68bde8859366dbbd8c9700a4b2ff79d02b3f35c
Burr Sutter <burrsutter@gmail.com>
2023-01-16 16:53:58 -0500

    also returning PORT

--- app.js
+++ app.js
@@ -7,20 +7,22 @@ let cnt = 0;
 
 // static page serving
 app.use(express.static(path.join(__dirname, 'html')));
+
+var PORT = process.env.PORT || 8080;
+
 app.get("/", async(req,res,next) => {
   res.render('index');
 })
 
 app.get("/hello", async(req, res) => {
 	try {
-		res.end(`Node Hello on ${os.hostname()}:${cnt++} \n`);
+		res.end(`Node Hello on ${os.hostname()}:${PORT} - ${cnt++} \n`);
 	} catch (err) {
 		console.error(err.message);
 	}  
 })
 
-var port = process.env.PORT || 8080;
 
-app.listen(port, function () {
-  console.log('my Node app listening on port ' + port + '!');
+app.listen(PORT, function () {
+  console.log('my Node app listening on PORT ' + PORT + '!');
 });
